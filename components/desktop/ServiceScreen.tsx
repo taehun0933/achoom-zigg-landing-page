76056b9ed66337934ea3e61f6eb41ed2413b22d0
taehun0933 <taehun0933@naver.com>
2024-10-29 15:49:09 +0900

    데스크탑 스페이스 스크린 애니메이션 적용 완료

--- components/desktop/ServiceScreen.tsx
+++ components/desktop/ServiceScreen.tsx
@@ -1,4 +1,45 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
 const ServiceScreen: React.FC = () => {
+  const spaceNameArrowRef = useRef<HTMLImageElement | null>(null);
+  const memberInviteArrowRef = useRef<HTMLImageElement | null>(null);
+  const [isSpaceNameVisible, setIsSpaceNameVisible] = useState(false);
+  const [isMemberInviteVisible, setIsMemberInviteVisible] = useState(false);
+
+  useEffect(() => {
+    const observerOptions = {
+      threshold: 0.1, // 요소가 10%만 뷰포트에 보여도 콜백이 실행
+    };
+
+    const spaceNameObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsSpaceNameVisible(true);
+        }
+      });
+    }, observerOptions);
+
+    const memberInviteObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsMemberInviteVisible(true);
+        }
+      });
+    }, observerOptions);
+
+    if (spaceNameArrowRef.current)
+      spaceNameObserver.observe(spaceNameArrowRef.current);
+    if (memberInviteArrowRef.current)
+      memberInviteObserver.observe(memberInviteArrowRef.current);
+
+    return () => {
+      spaceNameObserver.disconnect();
+      memberInviteObserver.disconnect();
+    };
+  }, []);
+
   return (
     <div className="relative w-full flex flex-col pt-40">
       <header className="w-full flex flex-col items-center mb-40">
@@ -15,11 +56,15 @@ const ServiceScreen: React.FC = () => {
             alt="spaceNameEnter"
             className="max-w-[500px]"
           />
-          {/* 스페이스명 입력 안내 섹션. 해당 영역까지 스크롤 시, 우측에서 fade-in 효과 적용 */}
           <img
             src="/images/spaceNameArrow.png"
             alt="spaceNameArrow"
-            className="max-w-[700px] absolute top-[8.5%] left-[45%]"
+            ref={spaceNameArrowRef}
+            className={`max-w-[700px] absolute top-[8.5%] left-[45%] transition-all duration-700 ${
+              isSpaceNameVisible
+                ? "transform translate-x-0 opacity-100"
+                : "transform translate-x-10 opacity-0"
+            }`}
           />
         </div>
 
@@ -31,8 +76,13 @@ const ServiceScreen: React.FC = () => {
           />
           <img
             src="/images/memberInviteArrow.png"
-            alt="spaceMemberInvite"
-            className="max-w-[400px] absolute top-[61%] right-[100%]"
+            alt="memberInviteArrow"
+            ref={memberInviteArrowRef}
+            className={`max-w-[400px] absolute top-[61%] right-[100%] transition-all duration-700 ${
+              isMemberInviteVisible
+                ? "transform translate-x-0 opacity-100"
+                : "transform -translate-x-10 opacity-0"
+            }`}
           />
         </div>
       </div>
