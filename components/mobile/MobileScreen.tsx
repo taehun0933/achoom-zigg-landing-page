9c2c5cae2206166a8963aeb5e1f8584994b16865
taehun0933 <taehun0933@naver.com>
2024-10-29 14:34:51 +0900

    모바일 임시 구현 완료

--- components/mobile/MobileScreen.tsx
+++ components/mobile/MobileScreen.tsx
@@ -2,9 +2,10 @@ import OnboardingScreen from "./OnBoardingScreen";
 
 const MobileScreen: React.FC = () => {
   return (
-    <>
-      <OnboardingScreen />
-    </>
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center">
+      모바일은 디자인 나오면 구현예정
+      {/* <OnboardingScreen /> */}
+    </div>
   );
 };
 
