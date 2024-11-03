cb4a0fd086e0004ee1f8252a1fb755e54ab1f4f5
taehun0933 <taehun0933@naver.com>
2024-11-03 16:09:58 +0900

    모바일 온보딩 스크린 구현 완료

--- components/mobile/MobileScreen.tsx
+++ components/mobile/MobileScreen.tsx
@@ -2,10 +2,9 @@ import OnboardingScreen from "./OnBoardingScreen";
 
 const MobileScreen: React.FC = () => {
   return (
-    <div className="w-[100vw] h-[100vh] flex justify-center items-center">
-      모바일은 디자인 나오면 구현예정
-      {/* <OnboardingScreen /> */}
-    </div>
+    <>
+      <OnboardingScreen />
+    </>
   );
 };
 
--- components/mobile/OnBoardingScreen.tsx
+++ components/mobile/OnBoardingScreen.tsx
@@ -1,15 +1,56 @@
+"use client";
+
 const OnboardingScreen: React.FC = () => {
   return (
-    <div className="relative w-full h-[100vh]">
+    <div className="relative w-full pt-[90px] flex flex-col items-center px-9">
+      <div className="flex flex-col max-w-[600px] mb-4">
+        <img
+          src="/images/mobile/onboardingLogo.png"
+          alt="background"
+          className="transition-all duration-500 w-[200px] sm:w-[250px] md:w-[300px]"
+        />
+        <div className="w-full font-medium text-sm leading-5 sm:text-base sm:leading-6 md:text-lg md:leading-7">
+          ZIGG는 공연예술 분야 및 메신저 앱에서 경험하던 불편함을 개선하고 영상
+          및 피드백을 한눈에 확인할 수 있으며, 더 나아가 피드백을 주고 받는 과정
+          속 유용한 도구들을 제공하는 서비스입니다.
+        </div>
+        <div className="relative flex w-full max-w-[500px] mt-2 gap-2 justify-end">
+          <img
+            src="/images/googlePlay.png"
+            alt="googlePlay"
+            className="w-28 md:w-32 pointer-events-auto cursor-pointer hover:scale-[102%]"
+            onClick={() => {
+              window.open(
+                "https://play.google.com/store/apps/details?id=com.achoom.ZIGG&pcampaignid=web_share",
+                "_blank",
+                "noopener,noreferrer"
+              );
+            }}
+          />
+          <img
+            src="/images/appStore.png"
+            alt="appStore"
+            className="w-24 sm:w-28 md:w-32 pointer-events-auto cursor-pointer hover:scale-[102%]"
+            onClick={() => {
+              window.open(
+                "https://apps.apple.com/kr/app/zigg/id6670358731",
+                "_blank",
+                "noopener,noreferrer"
+              );
+            }}
+          />
+        </div>
+      </div>
       <img
         src="/images/background.png"
         alt="background"
-        className="w-full absolute"
+        className="absolute w-[100vw] top-[45%] left-0 -z-10"
       />
+
       <img
         src="/images/onboarding_mockup.png"
         alt="mockup"
-        className="w-1/3 absolute right-[10%] top-[20%]"
+        className="w-full max-w-[600px] mt-6"
       />
     </div>
   );
