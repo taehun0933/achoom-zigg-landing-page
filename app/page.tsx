1f7361c688f41111ff1c3d76677b0f9a931a7dbd
taehun0933 <taehun0933@naver.com>
2024-11-04 21:15:44 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import DesktopScreen from "@/components/desktop/DesktopScreen";
 import MobileScreen from "@/components/mobile/MobileScreen";
 
 export default function Home() {
+  console.log("test");
+
   return (
     <div>
       <div className="block lg:hidden">
